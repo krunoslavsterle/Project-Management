298668f0a9bd99143b1debb333bd111e181ec862
Krunster <krunoslavsterle@outlook.com>
2016-05-31 22:58:17 +0200

    - added theme, scripts, _Layout view

---
+++ ProjectManagment/PM.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Optimization;
+
+namespace PM.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
+                "~/Scripts/jquery/jquery.min.js",
+                "~/Scripts/jquery/jquery.nicescroll.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(               
+               "~/Scripts/bootstrap/bootstrap.js",
+               "~/Scripts/bootstrap/bootstrap-progressbar.min.js"               
+               ));
+
+            bundles.Add(new ScriptBundle("~/bundles/customs/js").Include(
+                "~/Scripts/icheck.min.js",
+                "~/Scripts/custom.js",
+                "~/Scripts/nprogress.js",
+                "~/Scripts/shCore.js",
+                "~/Scripts/shBrushXml.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include(
+                "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/theme/css").Include(
+                "~/Content/animate.css",
+                "~/Content/custom.css",
+                "~/Content/docs.css",
+                "~/Content/shCoreDefault.css"));
+        }
+    }
+}
--- ProjectManagment/PM.Web/Global.asax.cs
+++ ProjectManagment/PM.Web/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using PM.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace PM.Web
@@ -13,6 +15,7 @@ namespace PM.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
