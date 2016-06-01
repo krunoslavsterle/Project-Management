aaccd430cbdf82bd27df1b56b404590dccfbeb41
Krunster <krunoslavsterle@outlook.com>
2016-06-01 10:20:44 +0200

    - added Login and Register view-s

--- ProjectManagment/PM.Web/Controllers/HomeController.cs
+++ ProjectManagment/PM.Web/Controllers/HomeController.cs
@@ -13,5 +13,15 @@ namespace PM.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
