b28c41f76fef5c64f764f0795dc4be60d01cc264
Krunster <krunoslavsterle@outlook.com>
2016-06-12 11:25:55 +0200

    - implementing ASP.NET Identity

---
+++ ProjectManagment/PM.Web/App_Start/IdentityConfig.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using PM.Web.Identity.DAL;
+using PM.Web.Identity.Models;
+using System.Threading.Tasks;
+
+namespace PM.Web.App_Start
+{
+    /// <summary>
+    /// Application user manager class.
+    /// </summary>
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store)
+        {            
+        }
+
+        /// <summary>
+        /// Creates user manager.
+        /// </summary>
+        /// <param name="options">Options.</param>
+        /// <param name="context">IdentityDbContext.</param>
+        /// <returns>User manager.</returns>
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<PMIdentityDbContext>()));
+
+            // Configure validation logic for usernames.
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true              
+            };
+
+            // Configure validation logic for passwords.
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireNonLetterOrDigit = false,
+                RequireUppercase = false
+            };
+
+            // Register two factor authentication providers. This application uses Email as a step of receiving a code for verifying the user.
+            manager.RegisterTwoFactorProvider("EmailCode", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "Security Code",
+                BodyFormat = "Your security code is: {0}"
+            });
+
+            manager.EmailService = new EmailService();
+
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    /// <summary>
+    /// Email service class.
+    /// </summary>
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+}
---
+++ ProjectManagment/PM.Web/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PM.Web.Identity.Models
+{
+    /// <summary>
+    /// Application user model.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        /// <summary>
+        /// Creates a ClaimsIdentity representing the user.
+        /// </summary>
+        /// <param name="manager">User manager.</param>
+        /// <returns>ClaimsIdentity.</returns>
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+}
