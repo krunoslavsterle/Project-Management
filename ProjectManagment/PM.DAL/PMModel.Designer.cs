d6a84770115b58f140c100d00f200b8b98363137
Krunoslav <krunoslavsterle@gmail.com>
2016-12-10 17:51:23 +0100

    - fixes on data access

--- ProjectManagment/PM.DAL/PMModel.Designer.cs
+++ ProjectManagment/PM.DAL/PMModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Source repository\ASP.Net projects\ProjectManagment\ProjectManagment\PM.DAL\PMModel.edmx'. 
+﻿// T4 code generation is enabled for model 'G:\Source repository\ASP.Net projects\ProjectManagment\ProjectManagment\PM.DAL\PMModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
